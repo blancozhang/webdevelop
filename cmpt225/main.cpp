9083d53babf86343be3c5aa474290e889429400b
Lige Zhang <ligezhang@liges-macbook-pro.local>
2018-10-14 12:07:42 -0700

    Initial Commit

---
+++ cmpt225/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cmpt225
+//
+//  Created by Lige Zhang on 2018/10/14.
+//  Copyright © 2018年 Lige Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
